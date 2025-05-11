43659c838d59a32fb23547ca2e7731def35a38dd
subhanmeer <banimeer@gmail.com>
2025-05-11 16:16:50 -0700

    changes

--- frontend/src/environment.js
+++ frontend/src/environment.js
@@ -1,6 +1,6 @@
 let IS_PROD = true;
 const server = IS_PROD ?
-    "https://apnacollegebackend.onrender.com" :
+    "https://myvideocallbackend-git-main-subhans-projects-840f375c.vercel.app" :
 
     "http://localhost:8000"
 
