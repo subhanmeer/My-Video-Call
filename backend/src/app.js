1a56b9ff5ff30811ee394a30b527671c5567b496
subhanmeer <banimeer@gmail.com>
2025-05-11 17:08:09 -0700

    change

--- backend/src/app.js
+++ backend/src/app.js
@@ -14,7 +14,7 @@ const server = createServer(app);
 const io = connectToSocket(server);
 
 
-app.set("port", (process.env.PORT || 8000))
+app.set("port", (process.env.PORT || 8001))
 app.use(cors());
 app.use(express.json({ limit: "40kb" }));
 app.use(express.urlencoded({ limit: "40kb", extended: true }));
