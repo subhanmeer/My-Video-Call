e47d84950d938b474852fe976ea75e625d73ffc3
subhanmeer <banimeer@gmail.com>
2025-11-04 10:43:12 -0800

    .

--- frontend/src/environment.js
+++ frontend/src/environment.js
@@ -1,7 +1,6 @@
-const IS_PROD = true;
-
-const server = IS_PROD
-  ? "https://my-video-call-h35l.onrender.com"
-  : "http://localhost:8001";
+const server =
+  window.location.hostname === "localhost"
+    ? "http://localhost:5000"
+    : "https://my-video-call-h35l.onrender.com";
 
 export default server;
